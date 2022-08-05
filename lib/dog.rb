35261abb52e74db7ede643d2981c79c37141910a
Josh <jtsarvida@gmail.com>
2022-08-05 17:47:11 -0400

    commit tree 8b2453bae65c188086cbc0ba131b30fdeaf31dad

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
   # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+  def initialize(name, mood = "nervous", owner)
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
